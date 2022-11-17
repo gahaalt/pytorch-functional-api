28a4caff7b04c4b55e803130d02092569a4eb3a9
Szymon Mikler <sjmikler@gmail.com>
2022-11-17 11:01:14 +0100

    Remove all source

--- examples/__init__.py
+++
@@ -1,3 +0,0 @@
-#  Copyright (c) 2022 Szymon Mikler
-
-from . import common, encoder_decoder, resnet, vgg  # noqa: F401
--- pytorch_functional/__init__.py
+++ pytorch_functional/__init__.py
@@ -5,11 +5,5 @@ import logging
 logging.warning("Pytorch Functional was renamed to Pytorch Symbolic!")
 logging.warning("Please import pytorch_symbolic instead!")
 
-from pytorch_symbolic import (
-    Input,
-    SymbolicModel as FunctionalModel,
-    add_to_model,
-    useful_layers,
-    graph_algorithms,
-    config,
-)
+from pytorch_symbolic import *
+
